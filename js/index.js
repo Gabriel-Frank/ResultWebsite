a64e7f87cc750ec2fd76a24739f9d5ff94cec42d
Gabriel-Frank <68851149+Gabriel-Frank@users.noreply.github.com>
2020-09-24 13:36:12 +0100

    added files to repo

---
+++ js/index.js
@@ -0,0 +1,247 @@
+new Vue({
+    el: '#header',
+    data: {
+        Fname: '',
+        Sname: '',
+        RegNo: '',
+        on: true
+    },
+    methods: {
+        addName(){
+            if(this.Fname != 'Gabriel' || this.Sname != 'Frank' || this.RegNo != '123/01'){
+                this.Fname = '';
+                this.Sname = '';
+                this.RegNo = '';
+                alert('invalid student ID');
+            }
+            else{
+                this.on = false;
+            }
+        }
+    }
+})
+new Vue({
+    el: "#app",
+    data: {
+        name: "compilation sheet",
+        subject: "",
+        ca1: "",
+        ca2: "",
+        exam: "",
+        done: false,
+        total: ""
+    },
+    methods: {
+        add(){
+            this.total = (+this.ca1 + +this.ca2 + +this.exam)/ 3;
+            if(this.total >= 100 || this.subject == ""  || this.ca11 == ""  || this.ca12 == ""  || this.exam == "" ){
+                alert("invalid");
+                this.total = "";
+            } 
+            else{
+                this.done = true;
+                Math.floor(this.total); 
+            }
+        }
+    }
+})
+new Vue({
+    el: "#app2",
+    data: {
+        name: "compilation sheet",
+        subject: "",
+        ca1: "",
+        ca2: "",
+        exam: "",
+        done: false,
+        total: ""
+    },
+    methods: {
+        add(){
+            this.total = (+this.ca1 + +this.ca2 + +this.exam)/ 3;
+            if(this.total >= 100 || this.subject == ""  || this.ca11 == ""  || this.ca12 == ""  || this.exam == "" ){
+                alert("invalid");
+                this.total = "";
+            } 
+            else{
+                this.done = true;
+                Math.floor(this.total); 
+            }
+        }
+    }
+})
+new Vue({
+    el: "#app3",
+    data: {
+        name: "compilation sheet",
+        subject: "",
+        ca1: "",
+        ca2: "",
+        exam: "",
+        done: false,
+        total: ""
+    },
+    methods: {
+        add(){
+            this.total = (+this.ca1 + +this.ca2 + +this.exam)/ 3;
+            if(this.total >= 100 || this.subject == ""  || this.ca11 == ""  || this.ca12 == ""  || this.exam == "" ){
+                alert("invalid");
+                this.total = "";
+            } 
+            else{
+                this.done = true;
+                Math.floor(this.total); 
+            }
+        }
+    }
+})
+new Vue({
+    el: "#app4",
+    data: {
+        name: "compilation sheet",
+        subject: "",
+        ca1: "",
+        ca2: "",
+        exam: "",
+        done: false,
+        total: ""
+    },
+    methods: {
+        add(){
+            this.total = (+this.ca1 + +this.ca2 + +this.exam)/ 3;
+            if(this.total >= 100 || this.subject == ""  || this.ca11 == ""  || this.ca12 == ""  || this.exam == "" ){
+                alert("invalid");
+                this.total = "";
+            } 
+            else{
+                this.done = true;
+                Math.floor(this.total); 
+            }
+        }
+    }
+})
+new Vue({
+    el: "#app5",
+    data: {
+        name: "compilation sheet",
+        subject: "",
+        ca1: "",
+        ca2: "",
+        exam: "",
+        done: false,
+        total: ""
+    },
+    methods: {
+        add(){
+            this.total = (+this.ca1 + +this.ca2 + +this.exam)/ 3;
+            if(this.total >= 100 || this.subject == ""  || this.ca11 == ""  || this.ca12 == ""  || this.exam == "" ){
+                alert("invalid");
+                this.total = "";
+            } 
+            else{
+                this.done = true;
+                Math.floor(this.total); 
+            }
+        }
+    }
+})
+new Vue({
+    el: "#app6",
+    data: {
+        name: "compilation sheet",
+        subject: "",
+        ca1: "",
+        ca2: "",
+        exam: "",
+        done: false,
+        total: ""
+    },
+    methods: {
+        add(){
+            this.total = (+this.ca1 + +this.ca2 + +this.exam)/ 3;
+            if(this.total >= 100 || this.subject == ""  || this.ca11 == ""  || this.ca12 == ""  || this.exam == "" ){
+                alert("invalid");
+                this.total = "";
+            } 
+            else{
+                this.done = true;
+                Math.floor(this.total); 
+            }
+        }
+    }
+})
+new Vue({
+    el: "#app7",
+    data: {
+        name: "compilation sheet",
+        subject: "",
+        ca1: "",
+        ca2: "",
+        exam: "",
+        done: false,
+        total: ""
+    },
+    methods: {
+        add(){
+            this.total = (+this.ca1 + +this.ca2 + +this.exam)/ 3;
+            if(this.total >= 100 || this.subject == ""  || this.ca11 == ""  || this.ca12 == ""  || this.exam == "" ){
+                alert("invalid");
+                this.total = "";
+            } 
+            else{
+                this.done = true;
+                Math.floor(this.total); 
+            }
+        }
+    }
+})
+new Vue({
+    el: "#app8",
+    data: {
+        name: "compilation sheet",
+        subject: "",
+        ca1: "",
+        ca2: "",
+        exam: "",
+        done: false,
+        total: ""
+    },
+    methods: {
+        add(){
+            this.total = (+this.ca1 + +this.ca2 + +this.exam)/ 3;
+            if(this.total >= 100 || this.subject == ""  || this.ca11 == ""  || this.ca12 == ""  || this.exam == "" ){
+                alert("invalid");
+                this.total = "";
+            } 
+            else{
+                this.done = true;
+                Math.floor(this.total); 
+            }
+        }
+    }
+})
+new Vue({
+    el: "#app9",
+    data: {
+        name: "compilation sheet",
+        subject: "",
+        ca1: "",
+        ca2: "",
+        exam: "",
+        done: false,
+        total: ""
+    },
+    methods: {
+        add(){
+            this.total = (+this.ca1 + +this.ca2 + +this.exam)/ 3;
+            if(this.total >= 100 || this.subject == ""  || this.ca11 == ""  || this.ca12 == ""  || this.exam == "" ){
+                // alert("invalid");
+                this.total = "";
+            } 
+            else{
+                this.done = true;
+                Math.floor(this.total); 
+            }
+        }
+    }
+})
